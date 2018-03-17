cdc00f8a587f2898888bc3ef9ea67025f0e03cab
Thomas Gambet <thomas.gambet@gmail.com>
2018-03-17 15:02:44 +0100

    seal the route under /music/

--- src/main/scala/net/creasource/web/AudioLibraryRoutes.scala
+++ src/main/scala/net/creasource/web/AudioLibraryRoutes.scala
@@ -2,17 +2,21 @@ package net.creasource.web
 
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.settings.RoutingSettings
 import net.creasource.core.Application
+
 import scala.collection.JavaConverters._
 
 class AudioLibraryRoutes(application: Application) {
 
   val configLibraries: List[String] = application.config.getStringList("music.libraries").asScala.toList
 
+  implicit val settings: RoutingSettings = RoutingSettings.apply(application.config)
+
   def routes: Route =
     pathPrefix("music") {
       //onSuccess(application.libraryActor ? GetLibraries) { libraries =>
-      configLibraries.map(getFromBrowseableDirectory).fold(reject())(_ ~ _)
+      Route.seal(configLibraries.map(getFromBrowseableDirectory).fold(reject())(_ ~ _))
     }
 
 }

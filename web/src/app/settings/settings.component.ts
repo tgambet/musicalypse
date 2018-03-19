647687825bc0b5d27487245dd73ba45f0edfce6b
Thomas Gambet <thomas.gambet@gmail.com>
2018-03-19 18:35:51 +0100

    working on the settings page, add files to the drop zone

--- web/src/app/settings/settings.component.ts
+++ web/src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  files: File[] = [];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addFiles(files: FileList) {
+    _.forEach(files, (file: File) => {
+      const names = _.map(this.files, 'name');
+      if (!_.includes(names, file.name)) {
+        this.files.push(file);
+      }
+    });
+  }
+
+  cancel() {
+    this.files = [];
   }
 
 }

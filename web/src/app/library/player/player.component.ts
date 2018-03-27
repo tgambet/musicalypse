0732c7966c82f056d8a2d828734b9b9c558ec108
Thomas Gambet <thomas.gambet@gmail.com>
2018-03-27 17:58:11 +0200

    clean subscription

--- web/src/app/library/player/player.component.ts
+++ web/src/app/library/player/player.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import {MatDialog, MatSnackBar, MatTabGroup} from '@angular/material';
 import {BreakpointObserver} from '@angular/cdk/layout';
 import {AudioComponent} from '../../audio/audio.component';
@@ -6,13 +6,15 @@ import {LibraryService} from '../../services/library.service';
 import {FavoritesService} from '../../services/favorites.service';
 import {Track} from '../../model';
 import {DetailsComponent} from '../../dialogs/details/details.component';
+import {Subscription} from 'rxjs/Subscription';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent implements OnInit {
+export class PlayerComponent implements OnInit, OnDestroy {
 
   @Output()
   onPrevious: EventEmitter<void> = new EventEmitter();
@@ -26,6 +28,8 @@ export class PlayerComponent implements OnInit {
 
   smallScreen: boolean;
 
+  subscriptions: Subscription[] = [];
+
   constructor(
     private breakpointObserver: BreakpointObserver,
     public library: LibraryService,
@@ -37,9 +41,15 @@ export class PlayerComponent implements OnInit {
   ngOnInit() {
     this.audio = this.library.audio;
 
-    this.breakpointObserver.observe('(max-width: 599px)').subscribe(result => {
-      this.smallScreen = result.matches;
-    });
+    this.subscriptions.push(
+      this.breakpointObserver.observe('(max-width: 599px)').subscribe(result => {
+        this.smallScreen = result.matches;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    _.forEach(this.subscriptions, sub => sub.unsubscribe());
   }
 
   trackByURL(index: number, track: Track) {

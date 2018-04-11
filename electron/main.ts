61552efe53cc06bd1d0c533365a1ec4e20d557ee
Thomas Gambet <thomas.gambet@gmail.com>
2018-04-11 20:21:57 +0200

    allow only one instance of the application when run through electron

--- electron/main.ts
+++ electron/main.ts
@@ -17,145 +17,165 @@ serve = args.some(val => val === '--serve');
 
 function createWindow() {
 
-    const size = screen.getPrimaryDisplay().workAreaSize;
+  const size = screen.getPrimaryDisplay().workAreaSize;
 
-    // Create the browser window.
-    win = new BrowserWindow({
-        frame: false,
-        width: size.width - 200,
-        height: size.height - 200,
-        minHeight: 450,
-        minWidth: 350,
-        show: false,
-        backgroundColor: '#000'
-    });
+  // Create the browser window.
+  win = new BrowserWindow({
+    frame: false,
+    width: size.width - 200,
+    height: size.height - 200,
+    minHeight: 450,
+    minWidth: 350,
+    show: false,
+    backgroundColor: '#000'
+  });
 
-    if (serve) {
-        // dirname is target/electron/dist
-        require('electron-reload')(__dirname, {electron: require(`${__dirname}/../../../node_modules/electron`)});
-        win.loadURL('http://localhost:4200');
-    } else {
-        win.loadURL(url.format({
-            pathname: path.join(__dirname, '../../dist/electron/index.html'),
-            protocol: 'file:',
-            slashes: true
-        }));
-    }
+  if (serve) {
+    // dirname is target/electron/dist
+    require('electron-reload')(__dirname, {electron: require(`${__dirname}/../../../node_modules/electron`)});
+    win.loadURL('http://localhost:4200');
+  } else {
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, '../../dist/electron/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+  }
 
-    // win.webContents.openDevTools();
+  // win.webContents.openDevTools();
 
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store window
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null;
-    });
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store window
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
 
-    win.once('ready-to-show', () => {
-        win.show();
-    });
+  win.once('ready-to-show', () => {
+    win.show();
+  });
 
-    win.on('focus', () => {
-        win.send('focus', '');
-    });
+  win.on('focus', () => {
+    win.send('focus', '');
+  });
 
-    win.on('blur', () => {
-        win.send('blur', '');
-    });
+  win.on('blur', () => {
+    win.send('blur', '');
+  });
 
 }
 
 try {
 
-    let serverProcess: ChildProcess;
-
-    if (!serve) {
-
-        serverProcess = require('child_process')
-            .spawn(
-                'bin\\musicalypse.bat',
-                [],
-                {
-                    cwd: './target/universal/stage',
-                    env: {
-                        'JAVA_OPTS': '-Dmusic.uploadFolder=./uploads'
-                    }
-                }
-            ).addListener('exit', code => {
-                if (code === 1) {
-                    throw Error('Server exited unexpectedly.');
-                }
-            });
-
-        serverProcess.stdout.addListener('data', chunk => {
-           console.log(chunk.toString().replace(/\n$/, ''));
-        });
-
-        serverProcess.stderr.addListener('data', chunk => {
-            console.error(chunk.toString().replace(/\n$/, ''));
-        });
-
+  const shouldQuit = app.makeSingleInstance(() => {
+    if (win) {
+      if (win.isMinimized()) {
+        win.restore();
+      }
+      win.focus();
     }
+  });
 
-    const ipc = require('electron').ipcMain;
-    const dialog = require('electron').dialog;
+  let serverProcess: ChildProcess;
 
-    ipc.on('open-file-dialog', function (event) {
-        dialog.showOpenDialog({
-            properties: ['openDirectory']
-        }, function (directory) {
-            if (directory) {
-                event.sender.send('selected-directory', directory);
+  if (shouldQuit) {
+    app.quit();
+  } else {
+    if (!serve) {
+      serverProcess = require('child_process')
+        .spawn(
+          'bin\\musicalypse.bat',
+          [],
+          {
+            cwd: './target/universal/stage',
+            env: {
+              'JAVA_OPTS': '-Dmusic.uploadFolder=./uploads'
             }
+          }
+        ).addListener('exit', code => {
+          if (code === 1) {
+            throw Error('Server exited unexpectedly.');
+          }
         });
-    });
+      serverProcess.stdout.addListener('data', chunk => {
+        console.log(chunk.toString().replace(/\n$/, ''));
+      });
+      serverProcess.stderr.addListener('data', chunk => {
+        console.error(chunk.toString().replace(/\n$/, ''));
+      });
+    }
+  }
 
-    // This method will be called when Electron has finished
-    // initialization and is ready to create browser windows.
-    // Some APIs can only be used after this event occurs.
-    app.on('ready', () => {
-        if (serve) {
-            createWindow();
-        } else {
-            const waitOn = require('wait-on');
-            const opts = {
-                resources: ['http://127.0.0.1:8080/api/libraries/tracks'],
-                delay: 0,
-                interval: 200,
-                timeout: 10000,
-                headers: {'Accept': 'application/json'}
-            };
-            waitOn(opts, err => {
-                if (err) { throw err; }
-                createWindow();
-            });
-        }
-    });
+  const ipc = require('electron').ipcMain;
+  const dialog = require('electron').dialog;
 
-    // Quit when all windows are closed.
-    app.on('window-all-closed', () => {
-        // On OS X it is common for applications and their menu bar
-        // to stay active until the user quits explicitly with Cmd + Q
-        if (process.platform !== 'darwin') {
-            if (serverProcess) {
-                serverProcess.stdin.write('\n');
-                serverProcess.on('exit', app.quit);
-            } else {
-                app.quit();
-            }
-        }
+  ipc.on('open-file-dialog', function (event) {
+    dialog.showOpenDialog({
+      properties: ['openDirectory']
+    }, function (directory) {
+      if (directory) {
+        event.sender.send('selected-directory', directory);
+      }
     });
+  });
 
-    app.on('activate', () => {
-        // On OS X it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (win === null) {
-            createWindow();
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on('ready', () => {
+    if (serve) {
+      createWindow();
+    } else {
+      const waitOn = require('wait-on');
+      const opts = {
+        resources: ['http://127.0.0.1:8080/api/libraries/tracks'],
+        delay: 0,
+        interval: 200,
+        timeout: 10000,
+        headers: {'Accept': 'application/json'}
+      };
+      waitOn(opts, err => {
+        if (err) {
+          throw err;
         }
-    });
+        createWindow();
+      });
+    }
+  });
+
+  // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      if (serverProcess) {
+        serverProcess.stdin.write('\n');
+        serverProcess.on('exit', app.quit);
+      } else {
+        app.quit();
+      }
+    }
+  });
+
+  // app.on('will-quit', () => {
+  //   if (serverProcess) {
+  //     serverProcess.stdin.write('\n');
+  //     serverProcess.on('exit', app.quit);
+  //   } else {
+  //     app.quit();
+  //   }
+  // })
+
+  app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createWindow();
+    }
+  });
 
 } catch (e) {
-    app.quit();
-    throw e;
+  console.log(e);
+  app.exit(1);
 }

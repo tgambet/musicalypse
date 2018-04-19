dcc7ce7e6e529f023b16d248ddb95145835a33a9
Thomas Gambet <thomas.gambet@gmail.com>
2018-04-19 17:52:04 +0200

    setting dispatch to false on change theme effect

--- web/src/app/core/core.effects.ts
+++ web/src/app/core/core.effects.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {Actions, Effect, ofType} from '@ngrx/effects';
 import {Observable} from 'rxjs';
 import {map, tap} from 'rxjs/operators';
-import {Action} from '@ngrx/store';
 import {ChangeTheme, CoreActionTypes} from '@app/core/core.actions';
 import {OverlayContainer} from '@angular/cdk/overlay';
 import {PersistenceService} from '@app/core/services/persistence.service';
@@ -10,15 +9,15 @@ import {PersistenceService} from '@app/core/services/persistence.service';
 @Injectable()
 export class CoreEffects {
 
-  @Effect()
-  themes$: Observable<Action> =
+  @Effect({ dispatch: false })
+  themes$: Observable<void> =
     this.actions$.pipe(
       ofType<ChangeTheme>(CoreActionTypes.ChangeTheme),
       tap((action: ChangeTheme) => {
         this.overlayContainer.getContainerElement().className = 'cdk-overlay-container ' + action.payload.cssClass;
         PersistenceService.save('theme', JSON.stringify(action.payload));
       }),
-      map(() => ({type: '[Core] Null Action (theme changed)'}))
+      map(() => {})
     );
 
   constructor(

52fb8b579fe09d8d143807188e3e00f0904eefa1
Thomas Gambet <thomas.gambet@gmail.com>
2018-04-21 22:51:33 +0200

    reviewed controls component markup and css

--- web/src/app/library/components/shared/controls.component.ts
+++ web/src/app/library/components/shared/controls.component.ts
@@ -1,26 +1,27 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 
-// TODO review CSS: group controls.meta and form field and display as column
 @Component({
   selector: 'app-controls',
   template: `
     <div class="controls divider fake-scroll-y">
-      <button mat-button mat-icon-button class="back" (click)="backClicked.emit()" *ngIf="backButton">
+      <button mat-button mat-icon-button class="back" (click)="backClicked.emit()" *ngIf="backButton" >
         <mat-icon>arrow_back</mat-icon>
       </button>
-      <div class="controls-meta" [class.show]="!showSearch">
-        <ng-content></ng-content>
+      <div class="controls-inner">
+        <mat-form-field floatLabel="never" class="search" [class.showSearch]="showSearch">
+          <input #searchInput matInput title="Search" [(ngModel)]="search" (input)="searchChange.emit(search)" spellcheck="false">
+          <mat-placeholder>
+            <mat-icon class="search-icon">search</mat-icon>
+            {{ searchPlaceholder }}
+          </mat-placeholder>
+          <button mat-button *ngIf="search" matSuffix mat-icon-button aria-label="Clear" (click)="search=''; searchChange.emit(search)">
+            <mat-icon>close</mat-icon>
+          </button>
+        </mat-form-field>
+        <div class="controls-meta" [class.showSearch]="showSearch">
+          <ng-content></ng-content>
+        </div>
       </div>
-      <mat-form-field floatLabel="never" class="search" [class.show]="showSearch">
-        <input #searchInput matInput title="Search" [(ngModel)]="search" (input)="searchChange.emit(search)" spellcheck="false">
-        <mat-placeholder>
-          <mat-icon class="search-icon">search</mat-icon>
-          {{ searchPlaceholder }}
-        </mat-placeholder>
-        <button mat-button *ngIf="search" matSuffix mat-icon-button aria-label="Clear" (click)="search=''; searchChange.emit(search)">
-          <mat-icon>close</mat-icon>
-        </button>
-      </mat-form-field>
       <button mat-button mat-icon-button
               (click)="showSearch = !showSearch; showSearch ? searchInput.focus() : {}"
               class="searchButton">
@@ -42,32 +43,36 @@ import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@
       align-items: center;
       box-sizing: border-box;
     }
-    .controls-meta {
-      overflow: hidden;
-      height: 60px;
-      width: calc(100% - 40px - 2rem);
+    .controls-inner {
+      flex-grow: 1;
+      align-self: flex-end;
       display: flex;
       flex-direction: column;
-      justify-content: center;
-      position: absolute;
-      left: 1rem;
-      transform: translateY(60px);
+      transition-property: transform;
+      transition-timing-function: ease;
+      transition-duration: 0.4s;
+      overflow: hidden;
+    }
+    .controls-meta {
+      height: 60px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
       transition-property: transform;
       transition-timing-function: ease;
       transition-duration: 0.4s;
     }
-    .controls-meta.show {
-      transform: translateY(0);
+    .controls-meta.showSearch {
+      transform: translateY(60px);
     }
     .search {
-      flex-grow: 1;
-      transform: translateY(-60px);
+      height: 60px;
       transition-property: transform;
       transition-timing-function: ease;
       transition-duration: 0.4s;
     }
-    .search.show {
-      transform: translateY(0);
+    .search.showSearch {
+      transform: translateY(60px);
     }
     .search-icon {
       vertical-align: bottom;
@@ -79,17 +84,6 @@ import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@
     }
     .searchButton {
       margin-left: 1rem;
-    }
-    .back ~ .controls-meta {
-      left: calc(40px + 2rem);
-      width: calc(100% - 80px - 3rem);
-    }
-
-    @media screen and (min-width: 959px){
-      .back ~ .controls-meta {
-        left: 1rem;
-        width: calc(100% - 40px - 2rem);
-      }
     }
   `],
   changeDetection: ChangeDetectionStrategy.OnPush

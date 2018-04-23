b0a0d27caae94a2587e7bfd6539fa5729f35dc40
Thomas Gambet <thomas.gambet@gmail.com>
2018-04-23 13:46:48 +0200

    minor fixes

--- web/src/app/core/services/audio.service.ts
+++ web/src/app/core/services/audio.service.ts
@@ -1,6 +1,7 @@
 import {ElementRef, Injectable, Renderer2} from '@angular/core';
 import {Observable, Subject} from 'rxjs';
 import {throttleTime} from 'rxjs/operators';
+import {publishReplay, refCount} from 'rxjs/operators';
 
 /**
  * Service configured by CoreComponent and in charge of creating and setting up the audio tag.
@@ -44,10 +45,10 @@ export class AudioService {
   constructor () {
     this.volume$ = this._volume.asObservable();
     this.muted$ = this._muted.asObservable();
-    this.currentTime$ = this._currentTime.asObservable().pipe(throttleTime(200));
-    this.duration$ = this._duration.asObservable();
-    this.loading$ = this._loading.asObservable();
-    this.playing$ = this._playing.asObservable();
+    this.currentTime$ = this._currentTime.asObservable().pipe(publishReplay(1), refCount(), throttleTime(200));
+    this.duration$ = this._duration.asObservable().pipe(publishReplay(1), refCount());
+    this.loading$ = this._loading.asObservable().pipe(publishReplay(1), refCount());
+    this.playing$ = this._playing.asObservable().pipe(publishReplay(1), refCount());
     this.ended$ = this._ended.asObservable();
     this._volume.next(this.volume);
     this._muted.next(this.muted);

2c4d8b94af2fc9d99d7de520b1bdabbdb4b2a473
Thomas Gambet <thomas.gambet@gmail.com>
2018-05-21 19:44:13 +0200

    fixed player background border

--- web/src/app/player/player.component.ts
+++ web/src/app/player/player.component.ts
@@ -48,11 +48,10 @@ import {take, tap, mergeMap} from 'rxjs/operators';
     .player-background {
       position: fixed;
       z-index: 1;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      margin: -10px;
+      top: 1px;
+      left: 1px;
+      right: 1px;
+      bottom: 1px;
     }
     .b1, .b2 {
       position: absolute;

7a26f7fd017f04a64ba10d46125b204300a20524
Thomas Gambet <thomas.gambet@gmail.com>
2018-06-19 13:04:54 +0200

    single abstract methods

--- src/main/scala/net/creasource/web/package.scala
+++ src/main/scala/net/creasource/web/package.scala
@@ -19,54 +19,47 @@ package object web {
 
   trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
     implicit val jsonMessageFormat: RootJsonFormat[JsonMessage] = jsonFormat3(JsonMessage.apply)
-    implicit val httpHeaderWriter: RootJsonWriter[HttpHeader] = new RootJsonWriter[HttpHeader] {
-      override def write(obj: HttpHeader): JsValue = {
-        JsObject(
-          "name" -> JsString(obj.name()),
-          "value" -> JsString(obj.value())
-        )
-      }
+    implicit val httpHeaderWriter: RootJsonWriter[HttpHeader] = (obj: HttpHeader) => {
+      JsObject(
+        "name" -> JsString(obj.name()),
+        "value" -> JsString(obj.value())
+      )
     }
-    implicit val httpResponseFormat: RootJsonWriter[HttpResponse] = new RootJsonWriter[HttpResponse] {
-      override def write(obj: HttpResponse): JsValue =
-        JsObject(
-          "status"     -> JsNumber(obj.status.intValue),
-          "statusText" -> JsString(obj.status.reason),
-          "entity" -> (obj.entity match {
-            case HttpEntity.Strict(ct @ ContentTypes.`application/json`, body)  => JsonParser(body.decodeString(ct.charset.value))
-            case HttpEntity.Strict(ct @ ContentTypes.`text/plain(UTF-8)`, body) => JsString(body.decodeString(ct.charset.value))
-            case _ => throw new UnsupportedOperationException("Only strict application/json and text/plain endpoints are supported.")
-          })
-          //"headers" -> JsArray(obj.headers.map(_.toJson).toVector)
-        )
-    }
-    implicit val httpRequestFormat: RootJsonReader[HttpRequest] = new RootJsonReader[HttpRequest] {
-      override def read(json: JsValue): HttpRequest = {
-        val (method, uri, headers, entity) = json match {
-          case js: JsObject =>
-            val method = js.fields.get("method") match {
-              case Some(JsString("GET"))     => HttpMethods.GET
-              case Some(JsString("POST"))    => HttpMethods.POST
-              case Some(JsString("PUT"))     => HttpMethods.PUT
-              case Some(JsString("DELETE"))  => HttpMethods.DELETE
-              case Some(JsString("OPTIONS")) => HttpMethods.OPTIONS
-              case Some(JsString("HEAD"))    => HttpMethods.HEAD
-              case Some(m)                   => throw new UnsupportedOperationException(s"Method $m is not supported.")
-              case _                         => throw new UnsupportedOperationException(s"No Method header found.")
-            }
-            val uri = js.fields.get("url") match {
-              case Some(JsString(url)) => Uri(url)
-              case _ => throw new UnsupportedOperationException(s"No string url parameter found.")
-            }
-            val entity = js.fields.get("entity") match {
-              case None => HttpEntity.Empty
-              case Some(value: JsValue) => HttpEntity.Strict(ContentTypes.`application/json`, ByteString(value.compactPrint))
-            }
-            (method, uri, Nil, entity)
-          case _ => throw new UnsupportedOperationException("The body of an HttpRequest message must be a JsObject.")
-        }
-        HttpRequest(method = method, uri = uri, headers = headers, entity = entity)
+    implicit val httpResponseFormat: RootJsonWriter[HttpResponse] = (obj: HttpResponse) => JsObject(
+      "status" -> JsNumber(obj.status.intValue),
+      "statusText" -> JsString(obj.status.reason),
+      "entity" -> (obj.entity match {
+        case HttpEntity.Strict(ct@ContentTypes.`application/json`, body) => JsonParser(body.decodeString(ct.charset.value))
+        case HttpEntity.Strict(ct@ContentTypes.`text/plain(UTF-8)`, body) => JsString(body.decodeString(ct.charset.value))
+        case _ => throw new UnsupportedOperationException("Only strict application/json and text/plain endpoints are supported.")
+      })
+      //"headers" -> JsArray(obj.headers.map(_.toJson).toVector)
+    )
+    implicit val httpRequestFormat: RootJsonReader[HttpRequest] = (json: JsValue) => {
+      val (method, uri, headers, entity) = json match {
+        case js: JsObject =>
+          val method = js.fields.get("method") match {
+            case Some(JsString("GET")) => HttpMethods.GET
+            case Some(JsString("POST")) => HttpMethods.POST
+            case Some(JsString("PUT")) => HttpMethods.PUT
+            case Some(JsString("DELETE")) => HttpMethods.DELETE
+            case Some(JsString("OPTIONS")) => HttpMethods.OPTIONS
+            case Some(JsString("HEAD")) => HttpMethods.HEAD
+            case Some(m) => throw new UnsupportedOperationException(s"Method $m is not supported.")
+            case _ => throw new UnsupportedOperationException(s"No Method header found.")
+          }
+          val uri = js.fields.get("url") match {
+            case Some(JsString(url)) => Uri(url)
+            case _ => throw new UnsupportedOperationException(s"No string url parameter found.")
+          }
+          val entity = js.fields.get("entity") match {
+            case None => HttpEntity.Empty
+            case Some(value: JsValue) => HttpEntity.Strict(ContentTypes.`application/json`, ByteString(value.compactPrint))
+          }
+          (method, uri, Nil, entity)
+        case _ => throw new UnsupportedOperationException("The body of an HttpRequest message must be a JsObject.")
       }
+      HttpRequest(method = method, uri = uri, headers = headers, entity = entity)
     }
   }
 

62c4e52ff99893621e3f1282a7d67174f4c3cfe4
Thomas Gambet <thomas.gambet@gmail.com>
2018-06-29 14:55:36 +0200

    added a sharedSocket observable

--- web/src/app/core/services/loader.service.ts
+++ web/src/app/core/services/loader.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {concat, Observable, Subject, throwError} from 'rxjs';
-import {delay, publishReplay, refCount, retryWhen, take} from 'rxjs/operators';
-import {HttpSocketClientService} from '@app/core/services/http-socket-client.service';
+import {concat, EMPTY, Observable, Subject, throwError} from 'rxjs';
+import {catchError, delay, mergeMap, publishReplay, refCount, retryWhen, share, switchMap, take} from 'rxjs/operators';
+import {HttpSocketClientService, SocketMessage} from '@app/core/services/http-socket-client.service';
+import {MatSnackBar} from '@angular/material';
 
 @Injectable()
 export class LoaderService {
@@ -19,17 +20,59 @@ export class LoaderService {
   initializing$: Observable<boolean>;
   private initializing = new Subject<boolean>();
 
+  private socketObs$: Observable<SocketMessage>;
+
   constructor(
-    private httpSocketClient: HttpSocketClientService
+    private httpSocketClient: HttpSocketClientService,
+    private snack: MatSnackBar
   ) {
     this.loadings$ = this.loading.asObservable().pipe(publishReplay(1), refCount());
     this.initializing$ = this.initializing.asObservable().pipe(publishReplay(1), refCount());
+    // this.initializing$ = this.httpSocketClient.socketOpened$.pipe(map(opened => !opened), publishReplay(1), refCount())
     this.log$ = this.log.asObservable().pipe(publishReplay(1), refCount());
     this.hasErrors$ = this.hasErrors.asObservable().pipe(publishReplay(1), refCount());
     this.loadings$.subscribe();
     this.initializing$.subscribe();
     this.log$.subscribe();
     this.hasErrors$.subscribe();
+  }
+
+  /* errors => errors.pipe(
+      switchMap(() => {
+        if (window.navigator.onLine) {
+          console.warn(`WebSocket failed. Retrying in 500ms.`);
+          return timer(500);
+        } else {
+          return fromEvent(window, 'online').pipe(take(1));
+        }
+      }),
+    ))*/
+
+  getSharedSocket(): Observable<SocketMessage> {
+    if (!this.socketObs$) {
+      return this.socketObs$ =
+        this.initializing$.pipe(
+          switchMap(initializing => {
+            if (initializing) {
+              return EMPTY;
+            } else {
+              return this.httpSocketClient.getSocket().pipe(
+                retryWhen(errors =>
+                  concat(errors.pipe(delay(500), take(6)), throwError('Connection to server lost!'))
+                ),
+                catchError((error, caught) =>
+                  this.snack.open(error, 'Retry')
+                    .afterDismissed()
+                    .pipe(mergeMap(() => caught))
+                )
+              );
+            }
+          }),
+          share()
+        );
+    } else {
+      return this.socketObs$;
+    }
   }
 
   isLoading(): Observable<boolean> {
@@ -54,17 +97,16 @@ export class LoaderService {
     this.log.next('Loading...');
     this.httpSocketClient.getSocket().pipe(
       retryWhen(
-        errors => concat(errors.pipe(delay(500), take(10)), throwError('Connection to backend server failed!'))
+        errors => concat(errors.pipe(delay(500), take(8)), throwError('Connection to local server failed!'))
       ),
       take(1)
     ).subscribe(
-      () => {
-        this.initializing.next(false);
-      },
+      () => {},
       error => {
         this.hasErrors.next(true);
         this.log.next(error);
-      }
+      },
+      () => this.initializing.next(false)
     );
   }
 

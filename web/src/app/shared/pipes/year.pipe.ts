13eea45e129d327fde6f83a4880efe41f77414ea
Thomas Gambet <thomas.gambet@gmail.com>
2018-12-13 20:09:40 +0100

    fixed: allow null and undefined values to the year pipe

--- web/src/app/shared/pipes/year.pipe.ts
+++ web/src/app/shared/pipes/year.pipe.ts
@@ -6,7 +6,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class YearPipe implements PipeTransform {
 
   transform(value: string): string {
-    if (value.length > 4) {
+    if (value && value.length > 4) {
       return value.substr(0, 4);
     } else {
       return value;
